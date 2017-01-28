03c7e18edf8ace2d9d5cc582dc06a7fb2407c1d1
Bhavarsh Singh <bhavarshs@gmail.com>
2017-01-28 13:50:20 +0530

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,37 @@
 var socket = io();
 
+
+
+ function scrollToBottom() {
+         // Selectors
+        var messages = $('#messages');
+
+
+        var newMessage = messages.children('li:last-child');
+
+
+         // Heights
+
+         var clientHeight = messages.prop('clientHeight');
+
+
+         var scrollTop = messages.prop('scrollTop');
+
+         var scrollHeight = messages.prop('scrollHeight');
+        
+         var newMessageHeight = newMessage.innerHeight();
+
+         var lastMessageHeight = newMessage.prev().innerHeight();
+
+
+
+        if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+           
+           messages.scrollTop(scrollHeight);
+        }
+
+
+ }
 socket.on('connect',function (){
     console.log('Connected to server');
 })
@@ -26,6 +58,7 @@ socket.on('newMessage',function (msg){
 
 
     $('#messages').append(html);
+    scrollToBottom();
 
 })
 
@@ -52,6 +85,7 @@ socket.on('newLocationMessage',function(message){
 
 
     $('#messages').append(html);
+    scrollToBottom();
 
 
 
